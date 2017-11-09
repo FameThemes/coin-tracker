7095d2fa0d4539a6af317de4df0cd48d61b6ac46
TruongSa <shrimp2t@gmail.com>
2017-11-09 17:46:16 +0700

    Remove dupplicate

--- coin_tracker 2.js
+++
@@ -1,7 +0,0 @@
-jQuery( document ).ready( function( $ ){
-    $( ".datepicker" ).datepicker({
-        altFormat: "yy-mm-dd",
-        dateFormat: "yy-mm-dd",
-        altField: "#coin_tracker_date_add"
-    });
-} );
